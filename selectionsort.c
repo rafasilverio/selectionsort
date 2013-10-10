e73423cfb4a03dc8b0639a17a5872c3ed5cac008
rafasilverio <rafasilverio0@gmail.com>
2013-10-09 22:51:30 -0300

    Update selectionsort.c

--- selectionsort.c
+++ selectionsort.c
@@ -1,19 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**Define o tamanho de um vetor*/
 #define SIZE 200000
 
+/*Função que ordena o vetor selecionando o valor, conhecida como SelectionSort,
+  em relação ao BubbleSort ela faz menos alterações pelo fato de não trocar todo valor que encontrar,
+  pois é feito uma verificação antes da troca.*/
 void ordena(int *vetor, int tamanho){
  int i = 0, j;
  int menor, aux;
 
+ /*for para percorrer todos os dados do vetor*/
  for(i = 0; i  < tamanho-1; i++){
-  menor = i;
-  for(j = i+1; j < tamanho; j++){
+  menor = i; /*atribuição do menor valor*/
+  for(j = i+1; j < tamanho; j++){ /*percorre do menor ao final do vetor comparando e guardando o menor valor*/
     if(vetor[j] < vetor[menor])
      menor = j;    
   }
-  if(menor != i){
+  if(menor != i){ /*troca de posições*/
     aux = vetor[i];
     vetor[i] = vetor[menor];
     vetor[menor] = aux;
@@ -27,6 +32,7 @@ int main(){
 
   srand(0);
   
+  /*imprime o vetor de entrada*/
   printf("Normal\n");
   for(i = 0; i < SIZE; i++){
     vetor[i] = rand()%SIZE;
@@ -35,6 +41,7 @@ int main(){
  
   ordena(vetor, SIZE); 
 
+ /*imprime o vetor de ordenado*/
   printf("\nOrdenado\n");
   for(i = 0; i < SIZE; i++)
     printf("%d, ", vetor[i]);
